716dc18c0b3ffc27d93f20a374d3ce44a3041605
matthieu-27 <matthieu27@pm.me>
2025-10-17 10:21:36 +0200

    working but no disqualified

--- horse_race.py
+++ horse_race.py
@@ -18,26 +18,26 @@ def display_result(_horse_list):
     winners = 0
     for horse in _horse_list:
         progress = horse[1] // 30
-        progress_bar = '▓' * progress + '░' * (2500 // 30 - progress)
+        progress_bar = '▓' * progress + '░' * (2400 // 30 - progress)
         if count < len(_horse_list):
             if horse[1] == "DQ":
-                print(f"\033[91m{HORSE_STR + str(count)} - {horse[1]} : {progress_bar}\033[0m")
+                print(f"\033[91m{HORSE_STR + str(count + 1)} - {horse[1]} : {progress_bar}\033[0m")
             elif horse[1] >= MAX_DISTANCE and winners < game_mode:
-                print(f"\033[92m{HORSE_STR + str(count)} - {horse[1]} : {progress_bar}\033[0m")
+                print(f"\033[92m{HORSE_STR + str(count + 1)} - {horse[1]} : {progress_bar}\033[0m")
                 winners += 1
             else:
-                print(f'{HORSE_STR + str(count)} - {horse[1]} : {progress_bar}')
+                print(f'{HORSE_STR + str(count + 1)} - {horse[1]} : {progress_bar}')
             count += 1
 
 
 def check_distance(_distance):
     """ Returns True if parcoured distance is less than `MAX_DISTANCE`, False otherwise """
-    return True if _distance < MAX_DISTANCE else False
+    return True if _distance <= MAX_DISTANCE else False
 
 
-def check_speed(current_speed, next_speed):
+def check_speed(_turn_speed, _random_number):
     """ Returns True if not disqualified, False otherwise """
-    if current_speed == 6 and next_speed == 3:
+    if _turn_speed == 3 and _random_number == 6:
         return False
     return True
 
@@ -73,17 +73,21 @@ if __name__ == "__main__":
 
     while not game_over:
         result = []
+        disqualified = []
         for i in range(len(horses)):
             random_number = randrange(0, DICE)  # roll a number
-            speed, distance = horses[i][0], horses[i][1]  # get speed and sitance
+            speed, distance = horses[i][0], horses[i][1]
             turn_speed = SPEED_LIST[speed][random_number]  # find turn
-            if check_speed(i, turn_speed) and horses[i][1] != "DQ":
+            if check_speed(turn_speed, random_number):
                 horses[i][0] = turn_speed
                 horses[i][1] += get_turn_distance(turn_speed)
             else:
-                horses[i][1] = -1
+                disqualified.append(horses[i])
+                if len(disqualified) >= MIN_HORSES:
+                    game_over = True
+                    break
 
-            if not check_distance(distance) and len(result) < game_mode:
+            if not check_distance(distance) and len(result) <= game_mode:
                 result.append(f"{len(result) + 1}: {HORSE_STR + str(i + 1)} (distance : {get_distance(horses, i)})")
                 if len(result) == game_mode:
                     game_over = True
@@ -93,6 +97,8 @@ if __name__ == "__main__":
 
         if game_over:
             print(horses)
+            print(result)
             print('-' * 30)
             display_result(horses)
+            print(disqualified)
             break
